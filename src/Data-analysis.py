7a5d1d55e10c8dec8e694746ad39b4a86f8f7ac4
GioGOve <giole.gioele.governo@gmail.com>
2026-03-11 11:36:44 +0100

    Features DA

--- src/Data-analysis.py
+++ src/Data-analysis.py
@@ -1,4 +1,4 @@
-# %%
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -23,7 +23,7 @@ print(data.info())
 print("\nMissing values:")
 print(data.isna().sum())
 
-# %%
+
 ## First Engagement Analysis (likes)
 print("\nSummary statistics for likes:")
 print(data["likes"].describe())
@@ -36,7 +36,7 @@ plt.xlabel("Likes")
 plt.ylabel("Frequency")
 plt.show()
 
-# %%
+
 # Adjustment to the log transformation
 data["log_likes"] = np.log1p(data["likes"])
 
@@ -48,4 +48,55 @@ plt.xlabel("Log Likes")
 plt.ylabel("Frequency")
 plt.show()
 
-# %%
+
+
+## Features Analysis
+features = ["brightness", "sharpness", "width", "height"]
+print(data[features].describe())
+
+
+# Brightness Distribution
+plt.figure(figsize=(8,5))
+sns.histplot(data["brightness"], bins=40, kde=True)
+
+plt.title("Brightness Distribution")
+plt.xlabel("Brightness")
+plt.ylabel("Frequency")
+
+plt.show()
+
+# Sharpness Distribution
+plt.figure(figsize=(8,5))
+sns.histplot(data["sharpness"], bins=40, kde=True)
+
+plt.title("Sharpness Distribution")
+plt.xlabel("Sharpness")
+plt.ylabel("Frequency")
+
+plt.show()
+
+# Dimensions Distribution
+fig, axes = plt.subplots(1,2, figsize=(12,5))
+
+sns.histplot(data["width"], bins=40, ax=axes[0])
+axes[0].set_title("Image Width Distribution")
+
+sns.histplot(data["height"], bins=40, ax=axes[1])
+axes[1].set_title("Image Height Distribution")
+
+plt.show()
+
+
+# Aspect Ratio Analysis
+data["aspect_ratio"] = data["width"] / data["height"]
+
+
+# New Variable (Orientation)
+data["orientation"] = np.where(
+    data["height"] > data["width"],
+    "portrait",
+    np.where(data["width"] > data["height"], "landscape", "square")
+)
+
+# Orientation Distribution
+print(data["orientation"].value_counts())
